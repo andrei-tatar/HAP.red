fd88913115deaa9384bb306b69dde328fe0fa1c3
Andrei Tatar <ATATAR@isoftwaresystems.com>
2015-10-15 08:47:46 +0300

    merge web-in and web-out in web

--- nodes/web.js
+++ nodes/web.js
@@ -2,11 +2,8 @@ var socketio = require('socket.io');
 var EventEmitter = require('events');
 
 module.exports = function(RED) {
-    if (!RED.server.socketio)
-        RED.server.socketio = socketio(RED.server);
-    var io = RED.server.socketio;
-    
     var events = new EventEmitter();
+    var io = socketio(RED.server);
     io.on('connection', function(socket){
         socket.on('msg', function (msg) {
             events.emit('msg', msg, socket);
@@ -31,4 +28,29 @@ module.exports = function(RED) {
     }
     
     RED.nodes.registerType("web in", SocketIoInputNode);
+    
+    function SocketIoOutputNode(config) {
+        RED.nodes.createNode(this, config);
+        this.on('input', function(msg) {
+            var to;
+            
+            if (config.sendto === 'sender') {
+                var socket = io.sockets.connected[msg.socketio_id];
+                if (!socket)
+                    throw new Error("web: trying to send message to a missing or closed socket");
+                to = socket;
+            } 
+            else if (config.sendto === 'all') {
+                to = io;
+            } 
+            else return;
+            
+            to.emit('msg', {
+                event: msg.event || config.event, 
+                data: msg.payload 
+            });
+        });
+    }
+    
+    RED.nodes.registerType("web out", SocketIoOutputNode);
 };
